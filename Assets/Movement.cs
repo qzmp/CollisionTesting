0053e410692482a5b01a436ac7dcfe54a9f5481b
qzmp <lukasz.golebiowski@hotmail.com>
2019-04-24 16:49:07 +0200

    Initial commit

---
+++ Assets/Movement.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    public float movementSpeed = 10;
+    private Rigidbody rigidbody;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        this.rigidbody = GetComponent<Rigidbody>();    
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 movementVector = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+        this.rigidbody.AddForce(movementVector * this.movementSpeed, ForceMode.Acceleration);
+        
+    }
+}
---
+++ Assets/TerrainMovement.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+
+public class TerrainMovement : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        //this.transform.position = Vector3.zero;
+        //GetComponent<Rigidbody>().MovePosition(Vector3.zero);
+        MoveByDoTween();
+
+    }
+
+    private void MoveByDoTween()
+    {
+        GetComponent<Rigidbody>().DOMove(Vector3.zero, 1);
+    }
+
+    //private void MoveNormally()
+    //{
+
+    //}
+
+    //private IEnumerator Move(Vector3 targetPosition, float duration)
+    //{
+    //    while (duration > 0)
+    //    {
+    //        GetComponent<Rigidbody>().MovePosition()
+    //    }
+    //}
+
+
+}
